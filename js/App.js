52682f850562cb3c945c59f3bfcdd833efde309c
Abd Alwahab <bayanadam10@gmail.com>
2019-05-02 22:24:32 +0300

    update the navigation

---
+++ js/App.js
@@ -0,0 +1,31 @@
+$('.about--btn--js').click(function() {
+
+    $('html body').animate({scrollTop: $('.about-section').offset().top} , 1000);
+});
+
+
+$('.courses--btn--js').click(function() {
+
+    $('html body').animate({scrollTop: $('.cards-section').offset().top} , 1000)
+});
+
+
+$('.apps--btn--js').click(function() {
+
+    $('html body').animate({scrollTop: $('.apps-section').offset().top} , 1000)
+});
+
+
+$('.websites--btn--js').click(function() {
+
+    $('html body').animate({scrollTop: $('.reactapps').offset().top} , 1000)
+});
+
+
+
+$('.skills--btn--js').click(function() {
+
+    $('html body').animate({scrollTop: $('.feature-section').offset().top} , 1000)
+});
+
+
